7993a9f79ed9a53c6333401a390a90b440e2002d
fabio-martin <fabiomartin2006@gmail.com>
2021-06-18 12:26:25 +0400

    created windows service project and added custom log event

---
+++ MyFirstWindowsService/MyFirstWindowsService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWindowsService
+{
+    public partial class MyFirstWindowsService : ServiceBase
+    {
+        public MyFirstWindowsService()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource")) 
+            {
+                System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog"; 
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+
+        }
+    }
+}
